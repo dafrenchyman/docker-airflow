598ca67f2450e4abc3a06f0a76184f9d7cb86581
Kirill Blazhennov <kirill.blazhennov@appfolio.com>
2019-03-29 10:39:12 -0700

    auto-open browser

--- kubernetes/port_forwarding.sh
+++ kubernetes/port_forwarding.sh
@@ -9,3 +9,5 @@ rm nohup.out
 AIRFLOW_POD=`kubectl get pods | grep airflow-web -m 1| cut -f1 -d' '`
 AIRFLOW_PORT=`kubectl get pods ${AIRFLOW_POD} --template='{{(index (index .spec.containers 0).ports 0).containerPort}}{{"\n"}}'`
 nohup kubectl port-forward ${AIRFLOW_POD} ${AIRFLOW_PORT}:${AIRFLOW_PORT} &
+
+open http://localhost:${AIRFLOW_PORT}
